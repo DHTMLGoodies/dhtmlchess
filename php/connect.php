e3281db6fb8d1a9af162fdf9017472f5057803ff
DHTMLGoodies <post@dhtmlgoodies.com>
2012-12-18 02:08:24 +0100

    PHP code cleanup

---
+++ php/connect.php
@@ -0,0 +1,12 @@
+<?php
+
+$conn = mysql_connect($chess_db_host,$chess_db_username,$chess_db_password);
+if(!$conn){
+    die("Unable to connect $chess_db_host,$chess_db_username,$chess_db_password");
+}
+$db_selected = mysql_select_db($chess_db_database,$conn);
+if (!$db_selected) {
+    die ("Can't connect to " . $chess_db_database . " " . mysql_error());
+}
+
+
