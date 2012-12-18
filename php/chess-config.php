cfbd3b7ed7876c38f4a45968cfff0598d4bf190d
DHTMLGoodies <post@dhtmlgoodies.com>
2012-12-18 02:08:24 +0100

    PHP code cleanup

--- php/chess-config.php
+++ php/chess-config.php
@@ -2,14 +2,6 @@
 date_default_timezone_set('Europe/Berlin');
 define("CHESS_FILE_UPLOAD_URL", "/tmp/");
 
-$conn = mysql_connect($chess_db_host,$chess_db_username,$chess_db_password);
-if(!$conn){
-    die("Unable to connect $chess_db_host,$chess_db_username,$chess_db_password");
-}
-$db_selected = mysql_select_db($chess_db_database,$conn);;
-if (!$db_selected) {
-    die ("Can't connect to " . $chess_db_database . " " . mysql_error());
-}
 
 
 
--- php/database-connection.php
+++
@@ -1,6 +0,0 @@
-<?php
-if(!strstr($_SERVER['PHP_SELF'],"connect.php")){
-	$conn = mysql_connect("localhost","root","administrator");
-	mysql_select_db("dhtmlgds",$conn);
-}
-
