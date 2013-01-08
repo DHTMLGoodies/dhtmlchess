a3ff89469cae467b4e77d36fc86a5bc644615e3a
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-01-08 09:36:15 +0100

    Update php/dhtmlgoodies/chess-config.php

--- php/dhtmlgoodies/chess-config.php
+++ php/dhtmlgoodies/chess-config.php
@@ -1,7 +1,7 @@
 <?php
 date_default_timezone_set('Europe/Berlin');
 
-$conn = mysql_connect("localhost","dhtmlgds","Faas78D7");
+$conn = mysql_connect("localhost","root","administrator");
 mysql_select_db("dhtmlgds",$conn);
 
 define("CHESS_FILE_UPLOAD_URL", "tmp/");
