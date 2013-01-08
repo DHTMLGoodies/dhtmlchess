50bdef1479848931a985acceccf695b92b50c178
DHTMLGoodies <post@dhtmlgoodies.com>
2013-01-08 10:07:22 +0100

    Minor changes

--- php/dhtmlgoodies/chess-config.php
+++
@@ -1,7 +0,0 @@
-<?php
-date_default_timezone_set('Europe/Berlin');
-
-$conn = mysql_connect("localhost","root","administrator");
-mysql_select_db("dhtmlgds",$conn);
-
-define("CHESS_FILE_UPLOAD_URL", "tmp/");
