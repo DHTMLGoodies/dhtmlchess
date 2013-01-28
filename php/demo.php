f981844cb2446b9131f9eea01770af3382dd0536
DHTMLGoodies <post@dhtmlgoodies.com>
2013-01-28 18:00:03 +0100

    Updated chessDB

--- php/demo.php
+++ php/demo.php
@@ -7,10 +7,9 @@
  * To change this template use File | Settings | File Templates.
  */
 
-ini_set('display_errors','on');
+ini_set('display_errors', 'on');
 date_default_timezone_set("Europe/Berlin");
 require_once("autoload.php");
-
 
 
 LudoDB::setHost('localhost');
@@ -20,32 +19,38 @@ LudoDB::setDb('PHPUnit');
 
 header("Content-type: application/json");
 
+if (isset($_GET['fromCache'])) {
+    $game = new Game(2);
+    echo $game;
+} else {
 // Construct database tables
-$tables = array('Move','Game','Fen','Metadata','MetadataValue');
-foreach($tables as $table){
-    $inst = new $table;
-    $inst->drop();
-    $inst->createTable();
+    $tables = array('Move', 'Game', 'Fen', 'Metadata', 'MetadataValue');
+    foreach ($tables as $table) {
+        $inst = new $table;
+        $inst->drop()->yesImSure();
+        $inst->createTable();
+    }
+
+    $profiling = new Profiling('PGN to parser to DB');
+
+    LudoDB::enableLogging();
+
+    $parser = new PgnParser('chessDB/Tests/pgn/test.pgn');
+    $games = $parser->getGames();
+    foreach ($games as $gameData) {
+        $game = new Game();
+        $game->setDatabaseId(100);
+        $game->setFen($gameData['metadata']['fen']);
+        $game->setMetadata($gameData['metadata']);
+        $game->setMoves($gameData['moves']);
+        $game->commit();
+    }
+
+
+    $game = new Game($game->getId());
+
+    echo $game;
 }
 
-$profiling = new Profiling('PGN to parser to DB');
-
-LudoDB::enableLogging();
-
-$parser = new PgnParser('chessDB/Tests/pgn/test.pgn');
-$games = $parser->getGames();
-foreach($games as $gameData){
-    $game = new Game();
-    $game->setDatabaseId(100);
-    $game->setFen($gameData['metadata']['fen']);
-    $game->setMetadata($gameData['metadata']);
-    $game->setMoves($gameData['moves']);
-    $game->commit();
-}
-
-
-$game = new Game($game->getId());
-
-echo $game;
 
 #echo $profiling->end();
