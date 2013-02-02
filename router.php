3d80fb8bc70b5fa14e66dd0ac9881e19ad192e77
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-02 01:09:51 +0100

    updated chessDB and using static ip address in router

--- router.php
+++ router.php
@@ -1,13 +1,17 @@
 <?php
 
 require_once(__DIR__."/autoload.php");
+date_default_timezone_set("Europe/Berlin");
 
 /**
  * TODO move this code
  */
 LudoDB::setUser('root');
 LudoDB::setPassword('administrator');
-LudoDB::setHost('localhost');
+/**
+ * TODO Use ip address instead of localhost on Windows
+ */
+LudoDB::setHost('127.0.0.1');
 LudoDB::setDb('PHPUnit');
 LudoDB::enableLogging();
 
