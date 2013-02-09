22c000d9cb029af146b8fe54758bc47416d84d42
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-09 05:11:59 +0100

    File cleanup - removed old files

--- controller.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Chess controller when mod-rewrite is deactivated
- * User: Alf Magne Kalleland
- * Date: 08.02.13
- * Time: 20:07
- */
--- db-connection.php
+++
@@ -1,5 +0,0 @@
-<?php
-$chess_db_host='localhost';
-$chess_db_username='root';
-$chess_db_password='administrator';
-$chess_db_database='dhtml_chess';
