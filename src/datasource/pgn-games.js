2cbcec9a81a908c94342b21d9cccb0c9090035ee
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-02-11 23:01:50 +0100

    New data source for static pgn games

---
+++ src/datasource/pgn-games.js
@@ -0,0 +1,18 @@
+/**
+ * Data source for list of games in a static pgn file. An object of this class is automatically created
+ * by chess.view.gamelist.Grid
+ * @module DataSource
+ * @namespace chess.dataSource
+ * @class GameList
+ * @extends dataSource.Collection
+ */
+chess.dataSource.PgnGames = new Class({
+    Extends: ludo.dataSource.Collection,
+    type : 'chess.dataSource.GameList',
+    autoload:true,
+    singleton: true,
+    resource:'ChessFS',
+    service:"listOfGames",
+    "primaryKey":"index",
+    url:window.chess.URL
+});
