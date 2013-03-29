17ad410496340b194d5e3de34e3051efd9c345f5
Alf Magne Kalleland <post@dhtmlgoodies.com>
2013-03-29 13:29:10 +0100

    Added french and dutch translations

--- src/language/fr.js
+++ src/language/fr.js
@@ -1,10 +1,75 @@
 chess.language = Object.merge(chess.language, {
     pieces:{
         'pawn':'',
-        'knight':'C',
         'bishop':'F',
         'rook':'T',
+        'knight':'C',
         'queen':'D',
         'king':'R'
-    }
-});
+    },
+    'clear':'effacer',
+    'Good move':'Bon coup',
+    'Poor move':'Coup faible',
+    'Very good move':'Très bon coup',
+    'Very poor move':'Coup très faible',
+    'Questionable move':'Coup discutable',
+    'Speculative move':'Coup spéculatif',
+    'Position Setup':'Créer position',
+    'Castling':'Roquef',
+    'Side to move':'Couleur à jouer',
+    'Add comment before':'Ajouter commentaire avant',
+    'Add comment after':'Ajouter commentaire après',
+
+    'OK':'OK',
+    'Cancel':'Annuler',
+    'Sign in':'Se connecter',
+    'Sign out':'Se déconnecter',
+    'E-mail':'Adresse de courriel',
+    'Username':'Nom d'utilisateur',
+    'Full name':'Nom complet',
+    'Password':'Mot de passe',
+    'Repeat password':'Répétez le mot de passe',
+    'rememberMe':'Se souvenir de moi',
+    'register':'S'inscrire',
+    'invalidUserNameOrPassword':'Nom d'utilisateur ou mot de passe non valide',
+    'InvalidUsername':'Ce nom d'utilisateur existe déjà',
+    'invalidEmail':'Adresse de courriel invalide',
+    'My profile':'Mon profil',
+    'country':'Pays',
+    'Changes saved successfully':'Modifications enregistrées avec succès',
+    'Signed in as':'Connecté en tant que',
+
+    'Import games(PGN)':'Parties importées(PGN)',
+    'saveGame':'Sauvegarder Partie',
+    'Game':'Partie',
+
+    'tacticPuzzleSolvedTitle':'Bravo - Puzzle complet',
+    'tacticPuzzleSolvedMessage':'Bien fait! Vous avez résolu ce Puzzle. Cliquez sur OK pour charger le prochain problème',
+
+
+    'commandWelcome':'Tapez vos commandes. Pour l'aide, tapez aide (+ entrée).',
+    'command_help':'Affichage écran d'aide',
+    'command_move':'Tapez "coup + notation" ou notation seulement (p. ex. "e4"), pour ajouter des coups',
+    'command_cls':'Effacer l'écran',
+    'command_load':'Charger une partie spécifique avec cet id de la base de données',
+    'command_flip':'Tourner l'échiquier',
+    'command_grade':'Evaluez coup en cours',
+    'command_forward':'Allez au prochain coup',
+    'command_back':'Allez au coup précédent',
+    'command_fen':'Charge une position fen, par exemple "fen 6k1/8/6p1/8/8/1P6/2b5/5K2 w - - 0 1"',
+
+    "invalid game":"Partie non valide",
+    "invalid position":"Position non valide",
+    "invalid move":"Coup non valide",
+    "Moving":"En train de jouer",
+    "Move updated to":"coup bijgewerkt naar",
+    "Time":"Temps",
+    "From elo":"A partir d'elo",
+    "To elo":"Vers elo",
+    "Rated":"Classé",
+    "Pgn File" : "Fichier Pgn"
+};
+
+chess.getPhrase = function (phrase) {
+    return chess.language[phrase] !== undefined ? chess.language[phrase] : phrase;
+};
