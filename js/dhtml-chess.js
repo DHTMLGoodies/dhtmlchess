7f5884518940a1b9ba2b03552ec6fb1cea434db8
DHTMLGoodies <post@dhtmlgoodies.com>
2013-06-04 17:39:07 +0200

    Updated demo

--- js/dhtml-chess.js
+++ js/dhtml-chess.js
@@ -1,4 +1,4 @@
-/* Generated Tue Jun 4 17:24:49 CEST 2013 */
+/* Generated Tue Jun 4 17:38:49 CEST 2013 */
 /**
 DHTML Chess - Javascript and PHP chess software
 Copyright (C) 2012-2013 dhtml-chess.com
@@ -22797,6 +22797,66 @@ ludo.paging.PageInput = new Class({
 	insertJSON:function(){
 
 	}
+});/* ../ludojs/src/paging/current-page.js */
+/**
+ Displays current page number shown in a collection
+ @class paging.TotalPages
+ @extends View
+ @constructor
+ @param {Object} config
+ @example
+ children:[
+ ...
+ {
+			  type:'paging.TotalPages',
+			  dataSource:'myDataSource'
+		  }
+ ...
+ }
+ where 'myDataSource' is the id of a dataSource.Collection object used by a view.
+ */
+ludo.paging.CurrentPage = new Class({
+	Extends:ludo.View,
+	type:'grid.paging.CurrentPage',
+	width:25,
+	onLoadMessage:'',
+	/**
+	 * Text template for view. {pages} is replaced by number of pages in data source.
+	 * @attribute {String} tpl
+	 * @default '/{pages}'
+	 */
+	tpl:'{page}',
+
+	ludoDOM:function () {
+		this.parent();
+		this.getEl().addClass('ludo-paging-text');
+		this.getEl().addClass('ludo-paging-current-page');
+	},
+
+	ludoEvents:function () {
+		this.parent();
+        this.dataSourceEvents();
+	},
+
+    dataSourceEvents:function(){
+        if(ludo.get(this.dataSource)){
+            var ds = this.getDataSource();
+            if (ds) {
+                ds.addEvent('page', this.setPageNumber.bind(this));
+                this.setPageNumber(ds.getPageNumber());
+            }
+        }else{
+            this.dataSourceEvents.delay(100, this);
+        }
+    },
+
+	setPageNumber:function () {
+		this.setHtml(this.tpl.replace('{page}', this.getDataSource().getPageNumber()));
+	},
+
+	insertJSON:function () {
+
+	}
 });/* ../ludojs/src/paging/total-pages.js */
 /**
  Displays number of pages in a data source
@@ -22829,6 +22889,7 @@ ludo.paging.TotalPages = new Class({
 
 	ludoDOM:function () {
 		this.parent();
+		this.getEl().addClass('ludo-paging-text');
 		this.getEl().addClass('ludo-paging-total-pages');
 	},
 
