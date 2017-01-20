c1081fe90175889746dd2892c6695cd4bb2d56f2
DHTMLGoodies <post@dhtmlgoodies.com>
2017-01-20 19:10:14 +0100

    bugfixes

--- js/dhtml-chess-minified.js
+++ js/dhtml-chess-minified.js
@@ -1,3 +1,18 @@
+/* Generated Fri Jan 20 19:10:12 CET 2017 */
+/**
+DHTML Chess - Javascript and PHP chess software
+Copyright (C) 2012-2017 dhtml-chess.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /*!
 Web Build: http://mootools.net/core/builder/e7289bd0058c6790cb2b769822285f97
 */
