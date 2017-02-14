a0736eef9b62554b457e61ba9dcee396bf365a9f
DHTMLGoodies <post@dhtmlgoodies.com>
2017-02-14 19:37:35 +0100

    updates Wordpress

--- js/dhtml-chess.js
+++ js/dhtml-chess.js
@@ -1,3 +1,18 @@
+/* Generated Tue Feb 14 19:37:33 CET 2017 */
+/**
+DHTML Chess - Javascript and PHP chess software
+Copyright (C) 2012-2017 dhtml-chess.com
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 /* ../ludojs/src/../mootools/MooTools-Core-1.6.0.js */
 /* MooTools: the javascript framework. license: MIT-style license. copyright: Copyright (c) 2006-2016 [Valerio Proietti](http://mad4milk.net/).*/
 /*!
