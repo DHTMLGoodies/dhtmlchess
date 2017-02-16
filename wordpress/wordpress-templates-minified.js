da9959c5916c9dd0dd2731bf30487ff3d3f59623
DHTMLGoodies <post@dhtmlgoodies.com>
2017-02-17 00:46:53 +0100

    layout updates wordpress

--- wordpress/wordpress-templates-minified.js
+++ wordpress/wordpress-templates-minified.js
@@ -1,4 +1,4 @@
-// (C) dhtmlchess.com, Alf Kalleland 2017-02-17 00:42:35
+// (C) dhtmlchess.com, Alf Kalleland 2017-02-17 00:46:36
 /*
  * Copyright ©2017. dhtmlchess.com. All Rights Reserved.
  * This is a commercial software. See dhtmlchess.com for licensing options.
