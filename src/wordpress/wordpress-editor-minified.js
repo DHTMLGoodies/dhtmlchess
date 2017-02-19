bb9ff5dd4019f57821077dc65a4290c117fd7ec4
DHTMLGoodies <post@dhtmlgoodies.com>
2017-02-19 03:14:23 +0100

    updates

--- src/wordpress/wordpress-editor-minified.js
+++ src/wordpress/wordpress-editor-minified.js
@@ -1,4 +1,4 @@
-// (C) dhtmlchess.com, Alf Kalleland 2017-02-19 02:35:11
+// (C) dhtmlchess.com, Alf Kalleland 2017-02-19 03:14:12
 /*
  * Copyright ©2017. dhtmlchess.com. All Rights Reserved.
  * This is a commercial software. See dhtmlchess.com for licensing options.
