971b5f0d5bdee0aee31680e44c4f276c0206ff44
DHTMLGoodies <post@dhtmlgoodies.com>
2017-02-21 23:39:07 +0100

    updates

--- wp-plugins/build-plugin.php
+++ wp-plugins/build-plugin.php
@@ -93,7 +93,7 @@ class Archiver
     private function build()
     {
 
-        echo getcwd()."<br>";
+        echo "CWD " . getcwd()."<br>";
         if(file_exists("dhtml_chess.zip"))unlink("dhtml_chess.zip");
         exec("rm -rf dhtml_chess");
         exec("mkdir dhtml_chess");
@@ -106,18 +106,69 @@ class Archiver
             echo $cmd."<br>";
             echo exec($cmd);
 
+
         }
 
 
+
         chdir("dhtml_chess");
+
+        $pwd = getcwd();
+        echo $pwd."<br>";
+
+        if(strstr($pwd, "wp-plugins")){
+
+            exec("rm -rf api/demo");
+            exec("rm -rf api/php/wordpress/wordpress");
+            exec("rm -rf api/php/profiling");
+            exec("rm -rf api/php/engine");
+            exec("rm -rf api/php/db");
+            exec("rm -rf api/php/parser/composer.phar");
+            exec("rm -rf api/php/chessFS/tests");
+            exec("rm -rf api/php/chessDB/eco");
+            exec("rm -rf api/php/chessDB/Tests");
+            exec("rm -rf api/php/chessDB/player");
+            exec("rm -rf api/php/chessDB/JSONConfig");
+            exec("rm -rf api/php/chessDB/game");
+            exec("rm -rf api/php/chessDB/ludoDB/docs");
+            exec("rm -rf api/php/chessDB/ludoDB/xhprof");
+            exec("rm -rf api/php/chessDB/ludoDB/examples");
+            exec("rm -rf api/php/chessDB/ludoDB/Tests");
+            exec("rm -rf api/php/chessDB/ludoDB/progress");
+            exec("rm -rf api/php/chessDB/*.php");
+            exec("rm -rf api/php/parser/test");
+            exec("rm -rf api/php/jsonwrapper");
+            exec("rm -rf api/pgn/Alekhine.pgn");
+            exec("rm -rf api/pgn/polgar.pgn");
+            exec("rm -rf api/pgn/polgar-parsed.pgn");
+            exec("rm -rf api/pgn/tactics.pgn");
+            exec("rm -rf api/pgn/test2.pgn");
+            exec("rm -rf api/pgn/Magnus-Carlsen.pgn");
+            exec("rm -rf api/pgn/outwcsc.pgn");
+            exec("rm -rf api/pgn/russian_chess.pgn");
+            exec("rm -rf api/pgn/Tal.pgn");
+            exec("rm -rf api/garbochess-engine");
+            exec("rm -rf api/garbochess");
+            exec("rm -rf api/router.php");
+            exec("rm -rf api/stockfish-js/example");
+
+        }else{
+            die("Not found");
+        }
+
+
+        unlink("../../zip/dhtml_chess.zip");
+
         $zipDestination = "../../zip/dhtml_chess.zip";
 
         HZip::zipDir(".", $zipDestination);
+
+        echo "<br>Size: " . filesize($zipDestination);
         exec("cp -f ".$zipDestination." ../../../wp-testing/dhtml_chess.zip");
 
 
         chdir("../");
-        exec("rm -rf dhtml_chess");
+        #exec("rm -rf dhtml_chess");
     }
 }
 
