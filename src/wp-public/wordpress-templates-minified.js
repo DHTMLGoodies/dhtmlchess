49b89c9b41cfb47f4910d72bb589a8408e185ade
DHTMLGoodies <post@dhtmlgoodies.com>
2017-02-27 01:19:38 +0100

    board resize bugfixes

--- src/wp-public/wordpress-templates-minified.js
+++ src/wp-public/wordpress-templates-minified.js
@@ -1,4 +1,4 @@
-// (C) dhtmlchess.com, Alf Kalleland 2017-02-27 01:18:36
+// (C) dhtmlchess.com, Alf Kalleland 2017-02-27 01:19:30
 /*
  * Copyright ©2017. dhtmlchess.com. All Rights Reserved.
  * This is a commercial software. See dhtmlchess.com for licensing options.
