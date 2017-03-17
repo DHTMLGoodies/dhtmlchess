b9a66ad9a5228ebdb90fd92af5d882fbab837671
DHTMLGoodies <post@dhtmlgoodies.com>
2017-03-17 18:15:09 +0100

    fixed problem with fen tags inside pgn - should not be parsed as short code

--- src/wordpress/wordpress-editor-minified.js
+++ src/wordpress/wordpress-editor-minified.js
@@ -1,4 +1,4 @@
-// (C) dhtmlchess.com, Alf Kalleland 2017-03-17 17:18:45
+// (C) dhtmlchess.com, Alf Kalleland 2017-03-17 18:14:46
 /*
  * Copyright ©2017. dhtmlchess.com. All Rights Reserved.
  * This is a commercial software. See dhtmlchess.com for licensing options.
